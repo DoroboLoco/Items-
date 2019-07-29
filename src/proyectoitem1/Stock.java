95197c646449ff9f504dcaa1bb5e729ecc12de1f
Punto Digital <Punto Digital@DESKTOP-M1FRA71>
2019-07-29 17:27:24 -0300

    Implemente un método que retorna nueva lista de items con cantidad menor al valor indicado por el usuario

--- src/proyectoitem1/Stock.java
+++ src/proyectoitem1/Stock.java
@@ -1,22 +1,35 @@
 
 package proyectoitem1;
 
-import java.util.List;
+import java.util.ArrayList;
 
 
 public class Stock {
-    List<ItemStock>items;
+    ArrayList<ItemStock>items;
     
-    List<ItemStock> getItems(){
+    ArrayList<ItemStock> getItems(){
     return this.items;
     }
 
-    void setItems(List<ItemStock> items) {
+    void setItems(ArrayList<ItemStock> items) {
     this.items = items;
     }
 
     
-    
+    public ArrayList coonstultarItemsFaltantes(int cantidadMaxima){
+        
+        ArrayList<ItemStock> nuevaLista = new ArrayList<>();
+        for(int i=0; i<items.size(); i++ ){
+        
+            if(items.get(i).cantidad<cantidadMaxima){
+            
+                nuevaLista.add(items.get(i));
+            }
+            
+        }
+        
+        return items;
+    }
 
 
 }
